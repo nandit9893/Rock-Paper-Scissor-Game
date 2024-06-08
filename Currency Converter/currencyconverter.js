93fa7d21cb4d5f3e91925b7ae288d09cf9b7eb52
NANDIT SHARMA <137511995+nandit9893@users.noreply.github.com>
2024-06-08 22:30:29 +0530

    Add files via upload

---
+++ Currency Converter/currencyconverter.js
@@ -0,0 +1,62 @@
+// Assuming countryList is defined in code.js
+
+const dropdowns = document.querySelectorAll(".dropdown select");
+const btn = document.querySelector("form button");
+const fromCurr = document.querySelector(".from select");
+const toCurr = document.querySelector(".to select");
+const msg = document.querySelector(".msg");
+
+document.addEventListener("DOMContentLoaded", () => {
+    populateDropdowns();
+    updateExchangeRate();
+});
+
+const populateDropdowns = () => {
+    for (let select of dropdowns) {
+        for (let currCode in countryList) {
+            let newOption = document.createElement("option");
+            newOption.innerText = currCode;
+            newOption.value = currCode;
+            if (select.name === "from" && currCode === "USD") {
+                newOption.selected = "selected";
+            } else if (select.name === "to" && currCode === "INR") {
+                newOption.selected = "selected";
+            }
+            select.append(newOption);
+        }
+        select.addEventListener("change", (evt) => {
+            updateFlag(evt.target);
+        });
+    }
+}
+
+const updateFlag = (element) => {
+    let currCode = element.value;
+    let countryCode = countryList[currCode];
+    let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`;
+    let img = element.parentElement.querySelector("img");
+    img.src = newSrc;
+};
+
+btn.addEventListener("click", (evt) => {
+    evt.preventDefault();
+    updateExchangeRate();
+});
+
+const updateExchangeRate = async () => {
+    let amount = document.querySelector(".amount input");
+    let amtVal = amount.value;
+    if (amtVal === "" || amtVal < 1) {
+        amtVal = 1;
+        amount.value = "1";
+    }
+
+    const fromCurrency = fromCurr.value;
+    const toCurrency = toCurr.value;
+    const URL = `https://v6.exchangerate-api.com/v6/740af18fcf3aa48b3819e24d/latest/${fromCurrency}`;
+    let response = await fetch(URL);
+    let data = await response.json();
+    let rate = data.conversion_rates[toCurrency];
+    let finalAmount = amtVal * rate;
+    msg.innerText = `${amtVal} ${fromCurrency} = ${finalAmount.toFixed(2)} ${toCurrency}`;
+}
